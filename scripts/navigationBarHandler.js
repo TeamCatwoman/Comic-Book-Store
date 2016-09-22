28bfd28ac2a02a9290d4fbc5f4957f88fcc646e9
TeodSt <teod_stoyanova@abv.bg>
2016-09-23 00:54:39 +0300

    Fixed dropdown menu

--- scripts/navigationBarHandler.js
+++ scripts/navigationBarHandler.js
@@ -1,11 +1,28 @@
 $(".navbar-nav").on("mouseover", function(event) {
     let target = event.target;
     $(target.parentElement).addClass("active");
+
+    if ($(target.parentElement).hasClass("dropdown")) {
+        $(target).next().css("display", "block");
+    }
 });
 
 $(".navbar-nav").on("mouseout", function(event) {
     let target = event.target;
     $(target.parentElement).removeClass("active");
+});
+
+$(".dropdown-menu").on("mouseover", function(event) {
+    let target = event.target;
+    debugger;
+    $(target.parentElement).addClass("active");
+    $(this).css("display", "block");
+});
+
+
+$(".dropdown-menu").on("mouseout", function(event) {
+    let target = event.target;
+    $(this).css("display", "none");
 });
 
 $(".navbar-nav").on("click", function(event) {
