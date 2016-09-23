6dc3873cc5cf543c7e5c4dbffa6d347bb40f065d
composer2 <composer2be@gmail.com>
2016-09-23 18:54:27 +0300

    added login ajax register ajax starting ot the register templates

--- scripts/dataServer.js
+++ scripts/dataServer.js
@@ -1,15 +1,61 @@
 var dataServer = (function () {
-    const USERNAME_STORAGE_KEY = 'username-key';
+    const USERNAME_STORAGE_KEY = 'username-key',
+        AUTH_KEY_STORAGE_KEY = 'auth-key-key';
 
     // start users
+    function userRegister(user) {
+        var register = new Promise(function (resolve, reject) {
+            var reqUser = {
+                username: user.username,
+                passHash: CryptoJS.SHA256(user.password).toString()
+            };
+
+            $.ajax({
+                url: 'api/users WE NEED SERVER TO WORK',
+                method: 'POST',
+                data: JSON.stringify(reqUser),
+                contentType: 'application/json',
+                success: function (user) {
+                    localStorage.setItem(USERNAME_STORAGE_KEY, user.username);
+                    localStorage.setItem(AUTH_KEY_STORAGE_KEY, user.authKey);
+                    resolve(user);
+                }
+            });
+        });
+        return register;
+    }
+
     function userLogin(user) {
-        localStorage.setItem(USERNAME_STORAGE_KEY, user);
-        return Promise.resolve(user);
+        var login = new Promise(function (resolve, reject) {
+            var reqUser = {
+                username: user.username,
+                passHash: CryptoJS.SHA1(user.password).toString()
+            };
+         // before encrypt   console.log(user);
+         //after encrypt   console.log(reqUser);
+            $.ajax({
+                url: 'api/auth WE NEED A SERVER TO WORK',
+                method: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(reqUser),
+                success: function (user) {
+                    localStorage.setItem(USERNAME_STORAGE_KEY, user.username);
+                    localStorage.setItem(AUTH_KEY_STORAGE_KEY, user.authKey);
+                    resolve(user);
+                }
+            });
+        });
+        return login;
     }
 
     function userLogout() {
-        localStorage.removeItem(USERNAME_STORAGE_KEY);
-        return Promise.resolve();
+        var logout = new Promise(function (resolve, reject) {
+            localStorage.removeItem(AUTH_KEY_STORAGE_KEY);
+            localStorage.removeItem(USERNAME_STORAGE_KEY);
+            resolve();
+        });
+
+        return logout;
     }
 
     function userGetCurrent() {
@@ -19,6 +65,7 @@ var dataServer = (function () {
 
     return {
         users: {
+            register: userRegister,
             login: userLogin,
             logout: userLogout,
             current: userGetCurrent
--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,28 @@ import { router } from './routing.js';
 
 $(() => { // on document ready
   const root = $('#root'),
-        navbar = root.find('#nav-login-form'),
-        mainNav = navbar.find('#main-nav'),
-        contentContainer = $('#container'),
-        loginForm = $('#login'),
-        logoutForm = $('#logout'),
-        registerForm=$('#register'),
-        usernameSpan = $('#span-username'),
-        usernameInput = $('#login-input');
-        
+    navbar = root.find('#nav-login-form'),
+    mainNav = navbar.find('#main-nav'),
+    contentContainer = $('#container'),
+    loginForm = $('#login'),
+    logoutForm = $('#logout'),
+    registerForm = $('#register'),
+    usernameSpan = $('#span-username'),
+    usernameInput = $('#login-input'),
+    userPassword=$('#login-password');
+
   router.init();
 
   // start login/logout
   navbar.on('click', '#btn-login', (ev) => {
-    let username = usernameInput.val() || 'anonymous';
-    dataServer.users.login(username)
+    
+    var user = {
+      username: usernameInput.val() || 'anonymous',
+      password: userPassword.val()
+    };
+    dataServer.users.login(user)
       .then((user) => {
-        usernameInput.val('')
+        usernameInput.val('');
         usernameSpan.text(user);
         loginForm.addClass('hidden');
         registerForm.addClass('hidden');
@@ -29,12 +34,23 @@ $(() => { // on document ready
 
   navbar.on('click', '#btn-logout', (ev) => {
     dataServer.users.logout()
-    .then(() => {
-      usernameSpan.text('');
-      loginForm.removeClass('hidden');
-      registerForm.removeClass('hidden');
-      logoutForm.addClass('hidden');
-    });
+      .then(() => {
+        usernameSpan.text('');
+        loginForm.removeClass('hidden');
+        registerForm.removeClass('hidden');
+        logoutForm.addClass('hidden');
+      });
   });
   // end login/logout
 });
+
+//start register
+$('#btn-register').on('click', function () {
+  var user = {
+    username: $('#tb-user').val(),
+    password: $('#tb-pass').val()
+  };
+  dataServer.users.register(user);
+  console.log(user);
+});
+//end register
