bd23af1d466cf64660154230ae58bfb5ec731830
composer2 <composer2be@gmail.com>
2016-09-23 15:52:28 +0300

    rearrange  folders added login input added register button bootstrap in dependencies changes on the html structure

---
+++ scripts/routing.js
@@ -0,0 +1,46 @@
+import { data } from './data.js';
+import { templateLoader as tl } from './template-loader.js';
+
+var router = (() => {
+    let navigo;
+    function init() {
+        navigo = new Navigo(null, false);
+
+        navigo
+        .on('home', () => {
+            Promise.all(['get the data', tl.loadTemplate('load the template by name')])
+                .then(([data, template])=> $('#atach to DOM').html(template(data)))
+                .catch(console.log);
+        })
+        .on('about', () => {
+            Promise.all(['get the data', tl.loadTemplate('load the template by name')])
+                .then(([data, template])=> $('#atach to DOM').html(template(data)))
+                .catch(console.log);
+        })
+        .on('contact', () => {
+            Promise.all(['get the data', tl.loadTemplate('load the template by name')])
+                .then(([data, template])=> $('#atach to DOM').html(template(data)))
+                .catch(console.log);
+        })
+        .on('register', () => {
+            Promise.all(['get the data', tl.loadTemplate('load the template by name')])
+                .then(([data, template])=> $('#atach to DOM').html(template(data)))
+                .catch(console.log);
+        })
+        .on('log-in', () => {
+            Promise.all(['get the data', tl.loadTemplate('load the template by name')])
+                .then(([data, template])=> $('#atach to DOM').html(template(data)))
+                .catch(console.log);
+        })
+        .on('log-out', () => {
+            Promise.all(['get the data', tl.loadTemplate('load the template by name')])
+                .then(([data, template])=> $('#atach to DOM').html(template(data)))
+                .catch(console.log);
+        }).resolve();
+    }
+    return {
+        init
+    };
+
+})();
+export { router };
---
+++ scripts/template-loader.js
@@ -0,0 +1,23 @@
+const templateLoader = (() => {
+    const cache = {};
+    function loadTemplate(templateName) {
+        return new Promise((resolve, reject) => {
+            if (cache[templateName]) {
+                resolve(cache[templateName]);
+            }
+            else {
+                $.get(`./templates/${templateName}.handlebars`)
+                    .done((data) => {
+                        let template = Handlebars.compile(data);
+                        cache[templateName] = template;
+                        resolve(template);
+                    })
+                    .fail(reject);
+            }
+        });
+    }
+    return {
+        loadTemplate
+    };
+})();
+export { templateLoader };
