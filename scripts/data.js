c37ab78a21936f41ef2e0f897302957c687c8d9c
TeodSt <teod_stoyanova@abv.bg>
2016-09-24 14:40:47 +0300

    Added Telerik BackEnd Services Config

--- scripts/data.js
+++ scripts/data.js
@@ -1,30 +1,50 @@
-function writeNewPost(uid, username, picture, title, body) {
-    // A post entry.
-    var postData = {
-        author: username,
-        uid: uid,
-        body: body,
-        title: title,
-        starCount: 0,
-        authorPic: picture
+(function() {
+    let everlive = new Everlive('co50xbssvfni5o0s');
+    let comicBook = everlive.data('Comic');
+
+    let comic = {
+        Title: 'Batman',
+        Author: 'Joker',
+        Year: '2000',
+        ImageSrc: 'https://s-media-cache-ak0.pinimg.com/originals/cb/72/02/cb720221d3b97df5f64c2644465ccb25.jpg',
+        Publisher: 'DC',
+        Description: 'Some descriptio'
     };
 
-    // Get a key for a new Post.
-    var newPostKey = firebase.database().ref().child('posts').push().key;
-
-    // Write the new post's data simultaneously in the posts list and the user's post list.
-    var updates = {};
-    updates['/posts/' + newPostKey] = postData;
-    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-    return firebase.database().ref().update(updates);
-}
-
-function writeUserData(userId, name, email, imageUrl) {
-    debugger;
-    firebase.database().ref('users/' + userId).set({
-        username: name,
-        email: email,
-        profile_picture: imageUrl
+    // Creating Objects
+    comicBook.create(comic).
+    then(function() {
+        console.log("Comic book created");
     });
-}
+
+    // we get all the comic books -> even the ones added from the site
+    $("#comics").on("click", function() {
+        comicBook.get()
+            .then(function(data) {
+                    showAllComics(data);
+                },
+                function(error) {
+                    alert(JSON.stringify(error));
+                });
+
+        function showAllComics(data) {
+            let img = $("<img />"),
+                paragraph = $("<p />"),
+                elements = data.result,
+                div = $("<div />");
+
+            for (let i = 0; i < data.count; i += 1) {
+                let newImg = img.clone(true),
+                    par = paragraph.clone(true);
+
+                newImg.attr("src", elements[i].ImageSrc);
+                par.html(`title: ${elements[i].Title}, Author: ${elements[i].Author}`);
+
+                div.append(newImg);
+                div.append(par);
+            }
+
+            $("#container").append(div);
+        }
+    });
+})();
--- scripts/main.js
+++ scripts/main.js
@@ -2,66 +2,66 @@ import { dataServer } from './dataServer.js';
 import { router } from './routing.js';
 
 $(() => { // on document ready
-  const root = $('#root'),
-    navbar = root.find('#nav-login-form'),
-    mainNav = navbar.find('#main-nav'),
-    contentContainer = $('#container'),
-    loginForm = $('#login'),
-    logoutForm = $('#logout'),
-    registerForm = $('#btn-register-form'),
-    registerButton=$('#btn-register'),
-    usernameSpan = $('#span-username'),
-    usernameInput = $('#login-input'),
-    userPassword=$('#login-password');
+    const root = $('#root'),
+        navbar = root.find('#nav-login-form'),
+        mainNav = navbar.find('#main-nav'),
+        contentContainer = $('#container'),
+        loginForm = $('#login'),
+        logoutForm = $('#logout'),
+        registerForm = $('#btn-register-form'),
+        registerButton = $('#btn-register'),
+        usernameSpan = $('#span-username'),
+        usernameInput = $('#login-input'),
+        userPassword = $('#login-password');
 
-  router.init();
+    router.init();
 
-  // start login/logout
-  navbar.on('click', '#btn-login', (ev) => {
-    var user = {
-      username: usernameInput.val() || 'anonymous',
-      password: userPassword.val()
-    };
-    dataServer.users.login(user)
-      .then((user) => {
-        usernameInput.val('');
-        usernameSpan.text(user);
-        loginForm.addClass('hidden');
-        registerForm.addClass('hidden');
-        logoutForm.removeClass('hidden');
-      });
-  });
+    // start login/logout
+    navbar.on('click', '#btn-login', (ev) => {
+        var user = {
+            username: usernameInput.val() || 'anonymous',
+            password: userPassword.val()
+        };
+        dataServer.users.login(user)
+            .then((user) => {
+                usernameInput.val('');
+                usernameSpan.text(user);
+                loginForm.addClass('hidden');
+                registerForm.addClass('hidden');
+                logoutForm.removeClass('hidden');
+            });
+    });
 
-  navbar.on('click', '#btn-logout', (ev) => {
-    dataServer.users.logout()
-      .then(() => {
-        usernameSpan.text('');
-        loginForm.removeClass('hidden');
-        registerForm.removeClass('hidden');
-        logoutForm.addClass('hidden');
-      });
-  });
-  // end login/logout
+    navbar.on('click', '#btn-logout', (ev) => {
+        dataServer.users.logout()
+            .then(() => {
+                usernameSpan.text('');
+                loginForm.removeClass('hidden');
+                registerForm.removeClass('hidden');
+                logoutForm.addClass('hidden');
+            });
+    });
+    // end login/logout
 });
 
 //start register
-$('#btn-register').on('click', function () {
-  var user = {
-    username: $('#tb-user').val(),
-    password: $('#tb-pass').val()
-  };
-  dataServer.users.register(user);
-  console.log(user);
+$('#btn-register').on('click', function() {
+    var user = {
+        username: $('#tb-user').val(),
+        password: $('#tb-pass').val()
+    };
+    dataServer.users.register(user);
+    console.log(user);
 });
 
-$('#root').on('click', '#btn-register-form', function (ev) {
-        $('#root').addClass(' blurred');
-        $('#root').addClass(' disabled-background');
-    });
+$('#root').on('click', '#btn-register-form', function(ev) {
+    $('#root').addClass('blurred');
+    $('#root').addClass('disabled-background');
+});
 
-$('#container').on('click','#btn-register', function (ev) {
-  $('#root').removeClass('blurred');
-  $('#root').removeClass('disabled-background');
-  $('#container').html(""); //TODO after successfull register go to home page
+$('#container').on('click', '#btn-register', function(ev) {
+    $('#root').removeClass('blurred');
+    $('#root').removeClass('disabled-background');
+    $('#container').html(""); //TODO after successfull register go to home page
 });
 //end register
