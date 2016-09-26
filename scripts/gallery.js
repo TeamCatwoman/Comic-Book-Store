295ef684a1c49f4ea31a33ae471f0993d9a8be72
composer2 <composer2be@gmail.com>
2016-09-26 20:40:57 +0300

    gallery is working need a proper pictures all files which are not in used r deleted

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,48 +1,50 @@
-//  $('#body').on('click', "#read-expand", function () {
+/*global blueimp, $ */
 
-  // // Load demo images from flickr:
-
-  //   $.ajax({
-  //     // Flickr API is SSL only:
-  //     // https://code.flickr.net/2014/04/30/flickr-api-going-ssl-only-on-june-27th-2014/
-  //     url: 'https://api.flickr.com/services/rest/',
-  //     data: {
-  //       format: 'json',
-  //       method: 'flickr.interestingness.getList',
-  //       api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line
-  //     },
-  //     dataType: 'jsonp',
-  //     jsonp: 'jsoncallback'
-  //   }).done(function (result) {
-  //     var linksContainer = $('#links')
-  //     var baseUrl
-  //     // Add the demo images as links with thumbnails to the page:
-  //     $.each(result.photos.photo, function (index, photo) {
-  //       baseUrl = 'https://farm' + photo.farm + '.static.flickr.com/' +
-  //         photo.server + '/' + photo.id + '_' + photo.secret
-  //       $('<a/>')
-  //         .append($('<img>').prop('src', baseUrl + '_s.jpg'))
-  //         .prop('href', baseUrl + '_b.jpg')
-  //         .prop('title', photo.title)
-  //         .attr('data-gallery', '')
-  //         .appendTo(linksContainer)
-  //     })
-  //   })
-  // });
+$(function () {
+  'use strict'
+$("#body").on('click','#read-expand',function (ev){
+  
+  // Load demo images from flickr:
+  $.ajax({
+    // Flickr API is SSL only:
+    // https://code.flickr.net/2014/04/30/flickr-api-going-ssl-only-on-june-27th-2014/
+    url: 'https://api.flickr.com/services/rest/',
+    data: {
+      format: 'json',
+      method: 'flickr.interestingness.getList',
+      api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line
+    },
+    dataType: 'jsonp',
+    jsonp: 'jsoncallback'
+  }).done(function (result) {
+    var linksContainer = $('#links');
+    var baseUrl;
+    // Add the demo images as links with thumbnails to the page:
+    $.each(result.photos.photo, function (index, photo) {
+      baseUrl = 'https://farm' + photo.farm + '.static.flickr.com/' +
+      photo.server + '/' + photo.id + '_' + photo.secret;
+      $('<a/>')
+        .append($('<img>').prop('src', baseUrl + '_s.jpg'))
+        .prop('href', baseUrl + '_b.jpg')
+        .prop('title', photo.title)
+        .attr('data-gallery', '')
+        .appendTo(linksContainer);
+    });
+  });
+});
 
   $('#borderless-checkbox').on('change', function () {
-    var borderless = $(this).is(':checked')
-    $('#blueimp-gallery').data('useBootstrapModal', !borderless)
-    $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', borderless)
-  })
+    var borderless = $(this).is(':checked');
+    $('#blueimp-gallery').data('useBootstrapModal', !borderless);
+    $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', borderless);
+  });
 
   $('#fullscreen-checkbox').on('change', function () {
-    $('#blueimp-gallery').data('fullScreen', $(this).is(':checked'))
-  })
+    $('#blueimp-gallery').data('fullScreen', $(this).is(':checked'));
+  });
 
   $('#image-gallery-button').on('click', function (event) {
-    event.preventDefault()
-    blueimp.Gallery($('#links a'), $('#blueimp-gallery').data())
-  })
-
-//  });
+    event.preventDefault();
+    blueimp.Gallery($('#links a'), $('#blueimp-gallery').data());
+  });
+});
