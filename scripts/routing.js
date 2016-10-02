ca31279cf228250c1b0f6e85d2f19c80752aaa01
TeodSt <teod_stoyanova@abv.bg>
2016-10-02 12:29:32 +0300

    Changed Navigo URL

--- scripts/routing.js
+++ scripts/routing.js
@@ -12,7 +12,7 @@ var router = (() => {
         controller;
 
     function init() {
-        navigo = new Navigo(null, false);
+        navigo = new Navigo("https://cdn.rawgit.com/TeamCatwoman/Comic-Book-Store/master/index.html", false);
         controller = new Controller();
 
         navigo
